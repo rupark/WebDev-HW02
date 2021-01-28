b2dbe1238eefca026e5a4b1754740850d4cb9276
rupark <33532219+rupark@users.noreply.github.com>
2021-01-27 19:22:32 -0500

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,6 +1,9 @@
 var mode = "add";
 var prev = 0;
 
+
+
+
 // Displays the given value
 function display(val) {
   document.getElementById('display').innerHTML = val;
@@ -80,3 +83,23 @@ function multiply(val) {
   display(parseFloat(prev) * parseFloat(val));
   prev = 0;
 }
+
+document.getElementById("7").onclick = function() {key_pressed(7)};
+document.getElementById("8").onclick = function() {key_pressed(8)};
+document.getElementById("9").onclick = function() {key_pressed(9)};
+document.getElementById("+=").onclick = function() {equals()};
+
+document.getElementById("4").onclick = function() {key_pressed(4)};
+document.getElementById("5").onclick = function() {key_pressed(5)};
+document.getElementById("6").onclick = function() {key_pressed(6)};
+document.getElementById("-").onclick = function() {set_mode('sub')};
+
+document.getElementById("1").onclick = function() {key_pressed(1)};
+document.getElementById("2").onclick = function() {key_pressed(2)};
+document.getElementById("3").onclick = function() {key_pressed(3)};
+document.getElementById("*").onclick = function() {set_mode('mult')};
+
+document.getElementById("C").onclick = function() {cleardis()};
+document.getElementById("0").onclick = function() {key_pressed(0)};
+document.getElementById(".").onclick = function() {decimal()};
+document.getElementById("/").onclick = function() {set_mode('div')};
